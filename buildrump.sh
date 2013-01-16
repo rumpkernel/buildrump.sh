2421c7e3c78279dc23cd76b3399e470f3d9e9e1e
Antti Kantee <pooka@iki.fi>
2013-01-16 02:57:33 +0200

    Add -P to skip build.sh completely if running script n times

--- buildrump.sh
+++ buildrump.sh
@@ -15,6 +15,11 @@ JNUM=4
 NBSRC_DATE=20121230
 NBSRC_SUB=0
 
+
+#
+# support routines
+#
+
 # the parrot routine
 die ()
 {
@@ -38,8 +43,150 @@ helpme ()
 	exit 1
 }
 
+#
+# toolchain creation helper routines
+#
+
+appendmkconf ()
+{
+	[ ! -z "${1}" ] && echo "${2}${3}=${1}" >> "${MYTOOLDIR}/mk.conf"
+}
+
+#
+# Not all platforms have  the same set of crt files.  for some
+# reason unbeknownst to me, if the file does not exist,
+# at least gcc --print-file-name just echoes the input parameter.
+# Try to detect this and tell the NetBSD makefiles that the crtfile
+# in question should be left empty.
+chkcrt ()
+{
+	tst=`cc --print-file-name=${1}.o`
+	up=`echo ${1} | tr [a-z] [A-Z]`
+	[ -z "${tst%${1}.o}" ] && echo "_GCC_CRT${up}=" >>"${MYTOOLDIR}/mk.conf"
+}
+
+#
+# Create tools and wrappers.  This can be skipped with -P.
+# Giving -P implies you know what you're doing.  It's useful for
+# 1) iteration speed on a slow-ish host
+# 2) making manual modification to the tools for testing and avoiding
+#    the script nuke them on the next iteration
+#
+# external toolchain links are created in the format that
+# build.sh expects.
+#
+# TODO?: don't hardcore this based on PATH
+# TODO2: cpp missing
+maketools ()
+{
+	local TOOLS='ar as ld nm objcopy objdump ranlib size strip'
+	local CC
+
+	# XXX: why can't all cc's that are gcc actually tell me
+	#      that they're gcc with cc --version?!?
+	if cc --version | grep -q 'Free Software Foundation'; then
+		CC=gcc
+	elif cc --version | grep -q clang; then
+		CC=clang
+		LLVM='-V HAVE_LLVM=1'
+	else
+		die Unsupported cc "(`which cc`)"
+	fi
+
+	#
+	# Check for GNU ld (as invoked by cc, since that's how the
+	# NetBSD Makefiles invoke it)
+	if ! cc -Wl,--version 2>&1 | grep -q 'GNU ld' ; then
+		die "GNU ld required (by NetBSD Makefiles)"
+	fi
+
+	#
+	# Perform some toolchain feature tests to determine what options
+	# we need to use for building.
+	#
+
+	cd ${OBJDIR}
+	#
+	# Try to test if cc supports -Wno-unused-but-set-variable.
+	# This is a bit tricky since apparently gcc doesn't tell it
+	# doesn't support it unless there is some other error to complain
+	# about as well.  So we try compiling a broken source file...
+	echo 'no you_shall_not_compile' > broken.c
+	${CC} -Wno-unused-but-set-variable broken.c > broken.out 2>&1
+	if ! grep -q Wno-unused-but-set-variable broken.out ; then
+		W_UNUSED_BUT_SET=-Wno-unused-but-set-variable
+	fi
+	rm -f broken.c broken.out
+
+	#
+	# Check if the linker supports all the features of the rump kernel
+	# component ldscript used for linking shared libraries.
+	# If not, build only static rump kernel components.
+	echo 'SECTIONS { } INSERT AFTER .data' > ldscript.test
+	echo 'int main(void) {return 0;}' > test.c
+	if ! cc test.c -Wl,-T ldscript.test; then
+		HASPIC='-V NOPIC=1'
+	fi
+	rm -f test.c a.out ldscript.test
+
+	#
+	# Create external toolchain wrappers.
+	mkdir -p ${MYTOOLDIR}/bin || die "cannot create ${MYTOOLDIR}"
+	for x in ${CC} ${TOOLS}; do
+		# ok, it's not really --netbsd, but let's make-believe!
+		tname=${MYTOOLDIR}/bin/${mach_arch}--netbsd${toolabi}-${x}
+		[ -f ${tname} ] && continue
+
+		printf '#!/bin/sh\nexec %s $*\n' ${x} > ${tname}
+		chmod 755 ${tname}
+	done
+
+	cat > "${MYTOOLDIR}/mk.conf" << EOF
+NOGCCERROR=1
+CPPFLAGS+=-I${DESTDIR}/include
+LIBDO.pthread=_external
+RUMPKERN_UNDEF=${RUMPKERN_UNDEF}
+EOF
+
+	appendmkconf "${W_UNUSED_BUT_SET}" "CFLAGS" +
+	appendmkconf "${EXTRA_CFLAGS}" "CFLAGS" +
+	appendmkconf "${EXTRA_LDFLAGS}" "LDFLAGS" +
+	appendmkconf "${EXTRA_AFLAGS}" "AFLAGS" +
+	appendmkconf "${RUMP_DIAGNOSTIC}" "RUMP_DIAGNOSTIC"
+	appendmkconf "${DBG}" "DBG"
+
+	chkcrt begins
+	chkcrt ends
+	chkcrt i
+	chkcrt n
+
+	# Run build.sh.  Use some defaults.
+	# The html pages would be nice, but result in too many broken
+	# links, since they assume the whole NetBSD man page set to be present.
+	cd ${SRCDIR}
+	${binsh} build.sh -m ${machine} -U -u \
+	    -D ${OBJDIR}/dest -O ${OBJDIR} \
+	    -T ${MYTOOLDIR} -j ${JNUM} ${LLVM} ${BEQUIET} ${HASPIC} \
+	    -V EXTERNAL_TOOLCHAIN=${MYTOOLDIR} -V TOOLCHAIN_MISSING=yes \
+	    -V MKGROFF=no \
+	    -V MKARZERO=no \
+	    -V NOPROFILE=1 \
+	    -V NOLINT=1 \
+	    -V USE_SSP=no \
+	    -V MKHTML=no -V MKCATPAGES=yes \
+	    -V MAKECONF="${MYTOOLDIR}/mk.conf" \
+	  tools
+	[ $? -ne 0 ] && die build.sh tools failed
+}
+
+
+#
+# BEGIN SCRIPT
+#
+
 DBG='-O2 -g'
-while getopts 'd:hj:o:qrs:' opt; do
+SKIPTOOLS=false
+while getopts 'd:hj:o:Pqrs:' opt; do
 	case "$opt" in
 	j)
 		JNUM=${OPTARG}
@@ -52,6 +199,11 @@ while getopts 'd:hj:o:qrs:' opt; do
 		;;
 	o)
 		OBJDIR=${OPTARG}
+		;;
+	P)
+		# "developer only", not mentioned in -h
+		echo 'You asked for it, you got it: skipping tool build'
+		SKIPTOOLS=true
 		;;
 	r)
 		RUMP_DIAGNOSTIC=no
@@ -140,7 +292,7 @@ case ${mach_arch} in
 	machine="evbarm"
 	mach_arch="arm"
 	toolabi="elf"
-	# XXX: assume at least armv6k due to some armv6 inaccuracy in NetBSD
+	# XXX: assume at least armv6k due to armv6 inaccuracy in NetBSD
 	EXTRA_CFLAGS='-march=armv6k'
 	EXTRA_AFLAGS='-march=armv6k'
 	;;
@@ -159,126 +311,9 @@ case ${mach_arch} in
 	EXTRA_AFLAGS='-m64'
 	;;
 esac
+[ -z "${machine}" ] && die script does not know machine \"${mach_arch}\"
 
-[ -z "${machine}" ] && die script needs knowledge of machine \"${mach_arch}\"
-
-#
-# create links to an external toolchain in the format that
-# build.sh expects.
-#
-# TODO?: don't hardcore this based on PATH
-# TODO2: cpp missing
-# XXX: why can't all cc's that are gcc actually tell me
-#      that they're gcc with cc --version?!?
-#
-TOOLS='ar as ld nm objcopy objdump ranlib size strip'
-if cc --version | grep -q 'Free Software Foundation'; then
-	CC=gcc
-elif cc --version | grep -q clang; then
-	CC=clang
-	LLVM='-V HAVE_LLVM=1'
-else
-	die Unsupported cc "(`which cc`)"
-fi
-
-#
-# Check for GNU ld (as invoked by cc, since that's how the
-# NetBSD Makefiles invoke it)
-if ! cc -Wl,--version 2>&1 | grep -q 'GNU ld' ; then
-	die "GNU ld required (by NetBSD Makefiles)"
-fi
-
-#
-# Perform some toolchain feature tests to determine what options
-# we need to use for building.
-#
-
-cd ${OBJDIR}
-#
-# Try to test if cc supports -Wno-unused-but-set-variable.
-# This is a bit tricky since apparently gcc doesn't tell it
-# doesn't support it unless there is some other error to complain
-# about as well.  So we try compiling a broken source file...
-echo 'no you_shall_not_compile' > broken.c
-${CC} -Wno-unused-but-set-variable broken.c > broken.out 2>&1
-if ! grep -q Wno-unused-but-set-variable broken.out ; then
-	W_UNUSED_BUT_SET=-Wno-unused-but-set-variable
-fi
-rm -f broken.c broken.out
-
-#
-# Check if the linker supports all the features of the rump kernel
-# component ldscript used for linking shared libraries.
-# If not, build only static rump kernel components.
-echo 'SECTIONS { } INSERT AFTER .data' > ldscript.test
-echo 'int main(void) {return 0;}' > test.c
-if ! cc test.c -Wl,-T ldscript.test; then
-	HASPIC='-V NOPIC=1'
-fi
-rm -f test.c a.out ldscript.test
-cd ${SRCDIR}
-
-#
-# Create external toolchain wrappers.
-mkdir -p ${MYTOOLDIR}/bin || die "cannot create ${MYTOOLDIR}"
-for x in ${CC} ${TOOLS}; do
-	# ok, it's not really --netbsd, but let's make-believe!
-	tname=${MYTOOLDIR}/bin/${mach_arch}--netbsd${toolabi}-${x}
-	[ -f ${tname} ] && continue
-
-	printf '#!/bin/sh\nexec %s $*\n' ${x} > ${tname}
-	chmod 755 ${tname}
-done
-export EXTERNAL_TOOLCHAIN="${MYTOOLDIR}"
-export TOOLCHAIN_MISSING=yes
-
-cat > "${MYTOOLDIR}/mk.conf" << EOF
-NOGCCERROR=1
-CPPFLAGS+=-I${DESTDIR}/include
-LIBDO.pthread=_external
-RUMPKERN_UNDEF=${RUMPKERN_UNDEF}
-EOF
-appendmkconf () {
-	[ ! -z "${1}" ] && echo "${2}${3}=${1}" >> "${MYTOOLDIR}/mk.conf"
-}
-appendmkconf "${W_UNUSED_BUT_SET}" "CFLAGS" +
-appendmkconf "${EXTRA_CFLAGS}" "CFLAGS" +
-appendmkconf "${EXTRA_LDFLAGS}" "LDFLAGS" +
-appendmkconf "${EXTRA_AFLAGS}" "AFLAGS" +
-appendmkconf "${RUMP_DIAGNOSTIC}" "RUMP_DIAGNOSTIC"
-appendmkconf "${DBG}" "DBG"
-
-#
-# Not all platforms have  the same set of crt files.  for some
-# reason unbeknownst to me, if the file does not exist,
-# at least gcc --print-file-name just echoes the input parameter.
-# Try to detect this and tell the NetBSD makefiles that the crtfile
-# in question should be left empty.
-chkcrt () {
-	tst=`cc --print-file-name=${1}.o`
-	up=`echo ${1} | tr [a-z] [A-Z]`
-	[ -z "${tst%${1}.o}" ] && echo "_GCC_CRT${up}=" >>"${MYTOOLDIR}/mk.conf"
-}
-chkcrt begins
-chkcrt ends
-chkcrt i
-chkcrt n
-
-# Use some defaults.
-# The html pages would be nice, but result in too many broken
-# links, since they assume the whole NetBSD man page set to be present.
-${binsh} build.sh -m ${machine} -U -u -D ${OBJDIR}/dest -O ${OBJDIR} \
-    -T ${MYTOOLDIR} -j ${JNUM} ${LLVM} ${BEQUIET} ${HASPIC} \
-    -V MKGROFF=no \
-    -V MKARZERO=no \
-    -V EXTERNAL_TOOLCHAIN=${EXTERNAL_TOOLCHAIN} \
-    -V NOPROFILE=1 \
-    -V NOLINT=1 \
-    -V USE_SSP=no \
-    -V MKHTML=no -V MKCATPAGES=yes \
-    -V MAKECONF="${MYTOOLDIR}/mk.conf" \
-    tools
-[ $? -ne 0 ] && die build.sh tools failed
+${SKIPTOOLS} || maketools
 
 RUMPTOOLS="${MYTOOLDIR}"
 RUMPMAKE="${RUMPTOOLS}/bin/nbmake-${machine}"
@@ -385,7 +420,7 @@ main()
 EOF
 
 # should do this properly
-${CC} -g -o rumptest test.c -I${DESTDIR}/include -Wl,--no-as-needed -Wl,--whole-archive -lrumpfs_kernfs -lrumpvfs -lrump  -lrumpuser -Wl,--no-whole-archive ${EXTRA_CFLAGS} -lpthread ${EXTRA_RUMPUSER} -L${DESTDIR}/lib -Wl,-R${DESTDIR}/lib
+cc -g -o rumptest test.c -I${DESTDIR}/include -Wl,--no-as-needed -Wl,--whole-archive -lrumpfs_kernfs -lrumpvfs -lrump  -lrumpuser -Wl,--no-whole-archive ${EXTRA_CFLAGS} -lpthread ${EXTRA_RUMPUSER} -L${DESTDIR}/lib -Wl,-R${DESTDIR}/lib
 ./rumptest || die test failed
 
 echo
