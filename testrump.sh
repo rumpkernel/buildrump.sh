ac318f21a80b124ba8d9a3b5b267ccf6213a4567
Antti Kantee <pooka@iki.fi>
2013-02-14 14:28:04 +0100

    Test networking (and the tests need a subdirectory ...)

--- testrump.sh
+++ testrump.sh
@@ -5,15 +5,19 @@
 
 testcommon='#include <sys/types.h>
 #include <inttypes.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 static void
-die(const char *reason)
+die(const char *fmt, ...)
 {
+	va_list va;
 
-	fprintf(stderr, "%s\\n", reason);
+	va_start(va, fmt);
+	vfprintf(stderr, fmt, va);
+	va_end(va);
 	exit(1);
 }
 '
@@ -130,11 +134,129 @@ EOF
 	echo Done
 }
 
+donet ()
+{
+
+	echo Networking test
+
+	echo ${testcommon} > nettest.c
+	cat >> nettest.c <<EOF
+#include <sys/types.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <rump/rump.h>
+#include <rump/rump_syscalls.h>
+#include <rump/netconfig.h>
+
+#define CONNPORT 135
+
+#define NOFAIL(a) do {int rv=a; if(rv==-1) die("%s: %d",#a,errno);} while (0)
+
+static void
+server(void)
+{
+	struct sockaddr_in sin;
+	int slen = sizeof(sin);
+	int s, s2;
+
+	rump_init();
+
+	/* configure interface using the portable interfaces */
+	NOFAIL(rump_pub_netconfig_ifcreate("shmif0"));
+	NOFAIL(rump_pub_netconfig_ifsetlinkstr("shmif0", "busmem"));
+	NOFAIL(rump_pub_netconfig_ipv4_ifaddr("shmif0",
+	    "1.0.0.1", "255.255.255.0"));
+
+	/* socket, bind, listen, accept.  standard sockets programming */
+	NOFAIL(s = rump_sys_socket(RUMP_PF_INET, RUMP_SOCK_STREAM, 0));
+	memset(&sin, 0, sizeof(sin));
+	sin.sin_family = RUMP_AF_INET;
+	sin.sin_port = htons(CONNPORT);
+	sin.sin_addr.s_addr = INADDR_ANY;
+	NOFAIL(rump_sys_bind(s, (struct sockaddr *)&sin, slen));
+	NOFAIL(rump_sys_listen(s, 37));
+
+#define TESTSTR "You feel like you've been here before --More--\n"
+	NOFAIL(s2 = rump_sys_accept(s, (struct sockaddr *)&sin, &slen));
+	rump_sys_write(s2, TESTSTR, sizeof(TESTSTR));
+}
+
+static void
+client(void)
+{
+	struct sockaddr_in sin;
+	char buf[1024];
+	ssize_t nn;
+	int s;
+
+	/* configure networking using the portable interfaces */
+	NOFAIL(rump_pub_netconfig_ifcreate("shmif0"));
+	NOFAIL(rump_pub_netconfig_ifcreate("shmif0"));
+	NOFAIL(rump_pub_netconfig_ifsetlinkstr("shmif0", "busmem"));
+	NOFAIL(rump_pub_netconfig_ipv4_ifaddr("shmif0",
+	    "1.0.0.2", "255.255.255.0"));
+
+	/* socket, connect.  standard sockets programming */
+	NOFAIL(s = rump_sys_socket(RUMP_PF_INET, RUMP_SOCK_STREAM, 0));
+	memset(&sin, 0, sizeof(sin));
+	sin.sin_family = RUMP_AF_INET;
+	sin.sin_port = htons(CONNPORT);
+	sin.sin_addr.s_addr = inet_addr("1.0.0.1");
+	NOFAIL(rump_sys_connect(s, (struct sockaddr *)&sin, sizeof(sin)));
+
+	if ((nn = rump_sys_read(s, buf, sizeof(buf))) <= 0)
+		die("reading socket failed");
+
+	if (strcmp(buf, TESTSTR) != 0)
+		die("didn't receive what was expected");
+}
+
+int
+main(int argc, char *argv[])
+{
+
+	/*
+	 * Bootstrap rump kernel.  it's so cheap that we might as well
+	 * do it here before checking the args.
+	 */
+	rump_init();
+
+	if (argc != 2)
+		die("need role");
+	if (strcmp(argv[1], "server") == 0)
+		server();
+	else if (strcmp(argv[1], "client") == 0)
+		client();
+	else
+		die("invalid role");
+	return 0;
+}
+EOF
+
+	set -x
+	cc -g -o nettest nettest.c -I${DESTDIR}/include -Wl,--no-as-needed \
+	    -Wl,--whole-archive -lrumpnet_shmif -lrumpnet_config	\
+	    -lrumpnet_netinet -lrumpnet_net -lrumpnet -lrump 	 	\
+	    -lrumpuser -Wl,--no-whole-archive ${EXTRA_CFLAGS} -lpthread	 \
+	    ${EXTRA_RUMPUSER} -L${DESTDIR}/lib -Wl,-R${DESTDIR}/lib
+	set +x
+	./nettest server &
+	./nettest client || die nettest client failed
+}
+
 alltests ()
 {
 
 	IFS=' '
 	cd ${OBJDIR}
 	doremote
+	donet
 	dofs
 }
