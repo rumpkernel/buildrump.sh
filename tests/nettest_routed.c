3573d99906cf8b1fcf72eb931937c89e56d71392
Antti Kantee <pooka@iki.fi>
2013-03-18 20:23:07 +0000

    treat errors during interface configuration as fatal

--- tests/nettest_routed.c
+++ tests/nettest_routed.c
@@ -1,49 +1,60 @@
 #include <sys/types.h>
 
 #include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include <rump/rump.h>
 #include <rump/netconfig.h>
 
 /* A very simple network with three nodes, one of which is the DFZ ;) */
 
+#define NOFAIL_RV(a) do{int rv=a;if(rv){printf("%s:%d",#a,rv);abort();}}while(0)
+
 static void
 config_server(void)
 {
+	int rv;
 
 	/* configure interface using the portable interfaces */
-	rump_pub_netconfig_ifcreate("shmif0");
-	rump_pub_netconfig_ifsetlinkstr("shmif0", "net1");
-	rump_pub_netconfig_ipv4_ifaddr("shmif0", "1.0.0.1", "255.255.255.0");
+	NOFAIL_RV(rump_pub_netconfig_ifcreate("shmif0"));
+	NOFAIL_RV(rump_pub_netconfig_ifsetlinkstr("shmif0", "net1"));
+	NOFAIL_RV(rump_pub_netconfig_ipv4_ifaddr("shmif0",
+	    "1.0.0.1", "255.255.255.0"));
 
-	rump_pub_netconfig_ipv4_gw("1.0.0.2");
+	NOFAIL_RV(rump_pub_netconfig_ipv4_gw("1.0.0.2"));
 }
 
 
 static void
 config_client(void)
 {
+	int rv;
 
 	/* configure networking using the portable interfaces */
-	rump_pub_netconfig_ifcreate("shmif0");
-	rump_pub_netconfig_ifsetlinkstr("shmif0", "net2");
-	rump_pub_netconfig_ipv4_ifaddr("shmif0", "1.0.1.1", "255.255.255.0");
+	NOFAIL_RV(rump_pub_netconfig_ifcreate("shmif0"));
+	NOFAIL_RV(rump_pub_netconfig_ifsetlinkstr("shmif0", "net2"));
+	NOFAIL_RV(rump_pub_netconfig_ipv4_ifaddr("shmif0",
+	    "1.0.1.1", "255.255.255.0"));
 
-	rump_pub_netconfig_ipv4_gw("1.0.1.2");
+	NOFAIL_RV(rump_pub_netconfig_ipv4_gw("1.0.1.2"));
 }
 
 static void
 config_router(void)
 {
+	int rv;
 
 	/* configure networking using the portable interfaces */
-	rump_pub_netconfig_ifcreate("shmif0");
-	rump_pub_netconfig_ifsetlinkstr("shmif0", "net1");
-	rump_pub_netconfig_ipv4_ifaddr("shmif0", "1.0.0.2", "255.255.255.0");
+	NOFAIL_RV(rump_pub_netconfig_ifcreate("shmif0"));
+	NOFAIL_RV(rump_pub_netconfig_ifsetlinkstr("shmif0", "net1"));
+	NOFAIL_RV(rump_pub_netconfig_ipv4_ifaddr("shmif0",
+	    "1.0.0.2", "255.255.255.0"));
 
-	rump_pub_netconfig_ifcreate("shmif1");
-	rump_pub_netconfig_ifsetlinkstr("shmif1", "net2");
-	rump_pub_netconfig_ipv4_ifaddr("shmif1", "1.0.1.2", "255.255.255.0");
+	NOFAIL_RV(rump_pub_netconfig_ifcreate("shmif1"));
+	NOFAIL_RV(rump_pub_netconfig_ifsetlinkstr("shmif1", "net2"));
+	NOFAIL_RV(rump_pub_netconfig_ipv4_ifaddr("shmif1",
+	    "1.0.1.2", "255.255.255.0"));
 }
 
 #include "nettest_base.c"
--- tests/nettest_simple.c
+++ tests/nettest_simple.c
@@ -1,18 +1,23 @@
 #include <sys/types.h>
 
 #include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include <rump/rump.h>
 #include <rump/netconfig.h>
+
+#define NOFAIL_RV(a) do{int rv=a;if(rv){printf("%s:%d",#a,rv);abort();}}while(0)
 
 static void
 config_server(void)
 {
 
 	/* configure interface using the portable interfaces */
-	rump_pub_netconfig_ifcreate("shmif0");
-	rump_pub_netconfig_ifsetlinkstr("shmif0", "busmem");
-	rump_pub_netconfig_ipv4_ifaddr("shmif0", "1.0.0.1", "255.255.255.0");
+	NOFAIL_RV(rump_pub_netconfig_ifcreate("shmif0"));
+	NOFAIL_RV(rump_pub_netconfig_ifsetlinkstr("shmif0", "busmem"));
+	NOFAIL_RV(rump_pub_netconfig_ipv4_ifaddr("shmif0",
+	    "1.0.0.1", "255.255.255.0"));
 }
 
 
@@ -21,9 +26,10 @@ config_client(void)
 {
 
 	/* configure networking using the portable interfaces */
-	rump_pub_netconfig_ifcreate("shmif0");
-	rump_pub_netconfig_ifsetlinkstr("shmif0", "busmem");
-	rump_pub_netconfig_ipv4_ifaddr("shmif0", "1.0.0.2", "255.255.255.0");
+	NOFAIL_RV(rump_pub_netconfig_ifcreate("shmif0"));
+	NOFAIL_RV(rump_pub_netconfig_ifsetlinkstr("shmif0", "busmem"));
+	NOFAIL_RV(rump_pub_netconfig_ipv4_ifaddr("shmif0",
+	    "1.0.0.2", "255.255.255.0"));
 }
 
 static void
