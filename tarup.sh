501a3a04e814070c878a8f5a509cf2f48d96abac
Jens Staal <staal1978@gmail.com>
2013-05-24 13:40:21 +0200

    implementing constructive feedback on tarup.sh

--- tarup.sh
+++ tarup.sh
@@ -3,31 +3,41 @@
 # Copyright (c) 2013 
 # Jens Staal <staal1978@gmail.com>
 #
-#######################################
-# This work 'as-is' we provide.       #
-# No warranty express or implied.     #
-#     We've done our best,            #
-#     to debug and test.              #
-# Liability for damages denied.       #
-#                                     #
-# Permission is granted hereby,       #
-# to copy, share, and modify.         #
-#     Use as is fit,                  #
-#     free or for profit.             #
-# These rights, on this notice, rely. #
-#######################################
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+
+# This script will generate a tar.bz2 archive of the current git checkout
 
 echo "Detecting buildrump.sh git revision"
 
 _revision=$(git rev-parse HEAD)
-_dest=$(dirname $PWD)
+_date=$(git show -s --format="%ci" ${_revision})
 
-if [ ${_revision} ]
+if [ -z "${_revision}" ]
 then
-  echo "buildrump.sh git revision is ${_revision}"
-else
   echo "Error: git revision could not be detected"
   exit
+else
+  echo "buildrump.sh git revision is ${_revision}"
 fi
 
 echo "Checking out cvs sources"
@@ -36,11 +46,22 @@ echo "Checking out cvs sources"
 
 echo "Checkout done"
 
+echo "Generating temporary directory to be compressed"
+rm -rf buildrump # starting fresh
+mkdir -p buildrump
+#directories
+cp -r {.git,brlib,examples,obj,rump,src,tests} buildrump/
+#files
+cp {AUTHORS,buildrump.sh,LICENSE,tarup.sh} buildrump/
+
+echo ${_revision} > buildrump/gitrevision
+echo ${_date} > buildrump/revisiondate
+
 echo "Compressing sources to a snapshot release"
 
-tar -cjf ${_dest}/buildrump-${_revision}.tar.bz2 * 
+tar -cjf buildrump-${_revision}.tar.bz2 buildrump
 
 echo "Congratulations! our archive should be
-      at ${_dest}/buildrump-${_revision}.tar.bz2"
+      at buildrump-${_revision}.tar.bz2"
       
       
