b54f953028abcb01f311e5e813a624a4a43c9ac2
Jens Staal <staal1978@gmail.com>
2013-07-17 13:49:27 +0200

    added support for the GNU gold linker

--- buildrump.sh
+++ buildrump.sh
@@ -158,6 +158,8 @@ probeld ()
 
 	if ${CC} -Wl,--version 2>&1 | grep -q 'GNU ld' ; then
 		LD_FLAVOR=GNU
+	if ${CC} -Wl,--version 2>&1 | grep -q 'GNU gold' ; then
+                LD_FLAVOR=GNU
 	elif ${CC} -Wl,--version 2>&1 | grep -q 'Solaris Link Editor' ; then
 		LD_FLAVOR=sun
 	else
