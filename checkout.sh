ce9d0c6168c5018d7a7535233b807ccd2fc2aa88
Antti Kantee <pooka@iki.fi>
2013-07-19 14:46:03 +0300

    share/mk improvement

--- checkout.sh
+++ checkout.sh
@@ -37,7 +37,7 @@
 # 3) push rumpkernel-netbsd-src
 # 4) push buildrump.sh
 #
-NBSRC_CVSDATE="20130719 1111UTC"
+NBSRC_CVSDATE="20130719 1145UTC"
 NBSRC_CVSFLAGS="-z3 \
     -d ${BUILDRUMP_CVSROOT:-:pserver:anoncvs@anoncvs.netbsd.org:/cvsroot}"
 
