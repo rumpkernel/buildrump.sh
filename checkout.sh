a53047d747dfd6d7d9a25b9d246cf2ea52cc5c63
Antti Kantee <pooka@iki.fi>
2013-07-27 20:40:13 +0300

    pull fixes for #33

--- checkout.sh
+++ checkout.sh
@@ -46,8 +46,8 @@ NBSRC_CVSFLAGS="-z3 \
 # Cherry-pick patches are not in $NBSRC_CVSDATE
 # the format is "date1:dir1 dir2 dir3 ...;date2:dir 4..."
 NBSRC_EXTRA='
-    20130722 1215UTC:
-	src/lib/librumphijack/hijack.c
+    20130727 1740UTC:
+	src/lib/librumphijack
 	src/lib/librumpuser/rumpuser_port.h'
 
 GITREPO='https://github.com/anttikantee/rumpkernel-netbsd-src'
