60def71ec4a4bdd99e7d6e8d38112350227af7fa
Antti Kantee <pooka@iki.fi>
2013-08-26 00:53:38 +0300

    check rumpclient_init() return value. issue #37, maybe

--- tests/simpleclient.c
+++ tests/simpleclient.c
@@ -7,7 +7,8 @@ int
 main()
 {
 
-	rumpclient_init();
+	if (rumpclient_init() == -1)
+		die("rumpclient init failed, error %d", errno);
 	if (rump_sys_getpid() < 2)
 		die("something went wrong! (\"what\" left as an exercise)");
 	rump_sys_reboot(0, NULL);
