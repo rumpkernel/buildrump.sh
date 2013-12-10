5fcf72b72d63d104f2e474e03b419a37175777e9
Antti Kantee <pooka@iki.fi>
2013-12-10 16:36:31 +0000

    cherry-pick fixes for ppc

--- checkout.sh
+++ checkout.sh
@@ -55,7 +55,8 @@ NBSRC_CVSFLAGS="-z3 -d ${BUILDRUMP_CVSROOT}"
 # Cherry-pick patches are not in $NBSRC_CVSDATE
 # the format is "date1:dir1 dir2 dir3 ...;date2:dir 4..."
 NBSRC_EXTRA='
-    20131209 2115UTC:
+    20131210 1500UTC:
+	src/sys/compat/linux/arch/powerpc
 	src/sys/kern/makesyscalls.sh
 	src/sys/rump/librump/rumpkern/rump_syscalls.c
 	src/sys/rump/librump/rumpvfs/Makefile.rumpvfs'
