d067d4908e961a59814519fca6fdacc8474e54d8
Justin Cormack <justin@specialbusservice.com>
2013-12-30 11:05:50 +0000

    FreeBSD needs to link against pthread

--- buildrump.sh
+++ buildrump.sh
@@ -901,6 +901,7 @@ evaltarget ()
 		;;
 	"freebsd")
 		RUMPKERN_UNDEF='-U__FreeBSD__'
+		${KERNONLY} || EXTRA_RUMPCLIENT='-lpthread'
 		;;
 	"linux")
 		RUMPKERN_UNDEF='-Ulinux -U__linux -U__linux__ -U__gnu_linux__'
