3e553a4bfdf86fc9b69ea049b4b2e68d02bfe299
Antti Kantee <pooka@iki.fi>
2014-04-02 20:10:12 +0000

    cherry-pick a bunch of fixes, including Android support

--- checkout.sh
+++ checkout.sh
@@ -54,7 +54,14 @@ NBSRC_CVSFLAGS="-z3 -d ${BUILDRUMP_CVSROOT}"
 
 # Cherry-pick patches are not in $NBSRC_CVSDATE
 # the format is "date1:dir1 dir2 dir3 ...;date2:dir 4..."
-NBSRC_EXTRA=''
+NBSRC_EXTRA='
+    20140402 2000UTC:
+	src/lib/librumpuser
+	src/lib/librumpclient
+	src/lib/librumphijack
+	src/sys/rump/include/rump/rump.h
+	src/sys/rump/net/lib/libvirtif
+	src/usr.bin/rump_allserver'
 
 GITREPO='https://github.com/rumpkernel/rumpkernel-netbsd-src'
 GITREPOPUSH='git@github.com:rumpkernel/rumpkernel-netbsd-src'
