091d3ba4aab8af37452390d933ec31512ebf5382
Antti Kantee <pooka@iki.fi>
2014-04-24 20:17:08 +0000

    cherry-pick ssp wrapper, hopefully fixing npfctl with older cc

--- checkout.sh
+++ checkout.sh
@@ -54,7 +54,9 @@ NBSRC_CVSFLAGS="-z3 -d ${BUILDRUMP_CVSROOT}"
 
 # Cherry-pick patches are not in $NBSRC_CVSDATE
 # the format is "date1:dir1 dir2 dir3 ...;date2:dir 4..."
-NBSRC_EXTRA=''
+NBSRC_EXTRA='
+    20140424 2015UTC:
+	src/include/ssp/string.h'
 
 GITREPO='https://github.com/rumpkernel/rumpkernel-netbsd-src'
 GITREPOPUSH='git@github.com:rumpkernel/rumpkernel-netbsd-src'
