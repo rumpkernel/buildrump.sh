ded9628f9987f82c3153c5e640cc81abf331a165
Antti Kantee <pooka@iki.fi>
2014-05-23 11:05:36 +0000

    cherry picking

--- checkout.sh
+++ checkout.sh
@@ -55,8 +55,9 @@ NBSRC_CVSFLAGS="-z3 -d ${BUILDRUMP_CVSROOT}"
 # Cherry-pick patches are not in $NBSRC_CVSDATE
 # the format is "date1:dir1 dir2 dir3 ...;date2:dir 4..."
 NBSRC_EXTRA='
-    20140522 1232UTC:
-	src/sys/rump/librump/rumpvfs/rump_vfs.c'
+    20140523 1105UTC:
+	src/sys/rump/librump/rumpvfs/rump_vfs.c
+	src/lib/librumpuser/rumpuser_sp.c'
 
 GITREPO='https://github.com/rumpkernel/rumpkernel-netbsd-src'
 GITREPOPUSH='git@github.com:rumpkernel/rumpkernel-netbsd-src'
