186a78e7c70664437366a966bba5dc74908f4a4f
Antti Kantee <pooka@iki.fi>
2014-07-09 13:26:08 +0000

    update listsrcdirs date for final changes

--- checkout.sh
+++ checkout.sh
@@ -50,7 +50,7 @@ NBSRC_CVSFLAGS="-z3 -d ${BUILDRUMP_CVSROOT}"
 
 # If set, timestamp for src/sys/rump/listsrcdir.  If unset,
 # NBSRC_CVSDATE is used.
-NBSRC_LISTDATE="20140708 1431UTC"
+NBSRC_LISTDATE="20140709 1230UTC"
 
 # Cherry-pick patches are not in $NBSRC_CVSDATE
 # the format is "date1:dir1 dir2 dir3 ...;date2:dir 4..."
