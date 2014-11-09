a86cbb95eb9bd05c3f91ad608d5e1e7b10b76ff6
Antti Kantee <pooka@iki.fi>
2014-11-09 17:23:56 +0000

    for previous, test for and descend into correct directory

--- buildrump.sh
+++ buildrump.sh
@@ -682,9 +682,9 @@ makebuild ()
 	    sys/rump/dev sys/rump/fs sys/rump/kern sys/rump/net
 	    sys/rump/include ${BRDIR}/brlib"
 
-	# sys/rump/man was added to ${SRCDIR} 11/2014
-	[ -d ${SRCDIR}/sys/rump/man ] \
-	    && appendvar DIRS_second ${SRCDIR}/sys/rump
+	# sys/rump/share was added to ${SRCDIR} 11/2014
+	[ -d ${SRCDIR}/sys/rump/share ] \
+	    && appendvar DIRS_second ${SRCDIR}/sys/rump/share
 
 	if [ ${MACHINE} = "i386" -o ${MACHINE} = "amd64" \
 	     -o ${MACHINE} = "evbearm-el" -o ${MACHINE} = "evbearm-eb" \
