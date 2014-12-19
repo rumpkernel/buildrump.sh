6982264103ef2eda32c4a3f90266fec7bad91a22
Antti Kantee <pooka@iki.fi>
2014-12-19 12:18:10 +0000

    add a common build script for Xen and baremetal
    initial experiment, usage a bit rough around the edges

---
+++ xenbaremetal.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+# build script for bare metal and Xen.  Obeys the following
+# env variables:
+#
+#	BUILDXENMETAL_MKCONF:	extra contents for mk.conf
+#	BUILDXENMETAL_PCI_P:	predicate for building PCI drivers
+#
+
+STDJ='-j4'
+RUMPSRC=rumpsrc
+
+while getopts '?qs:' opt; do
+	case "$opt" in
+	's')
+		RUMPSRC=${OPTARG}
+		;;
+	'q')
+		BUILD_QUIET=${BUILD_QUIET:=-}q
+		;;
+	'?')
+		exit 1
+	esac
+done
+shift $((${OPTIND} - 1))
+
+# the buildxen.sh is not as forgiving as I am
+set -e
+
+. ./buildrump.sh/subr.sh
+
+if git submodule status ${RUMPSRC} | grep -q '^-' ; then
+	git submodule update --init --recursive ${RUMPSRC}
+fi
+[ "$1" = "justcheckout" ] && { echo ">> $0 done" ; exit 0; }
+
+# build tools
+./buildrump.sh/buildrump.sh ${BUILD_QUIET} ${STDJ} -k \
+    -V MKPIC=no -s ${RUMPSRC} -T rumptools -o rumpobj -N \
+    -V RUMP_KERNEL_IS_LIBC=1 "$@" tools
+
+[ -n "${BUILDXENMETAL_MKCONF}" ] \
+    && echo "${BUILDXENMETAL_MKCONF}" >> rumptools/mk.conf
+
+RUMPMAKE=$(pwd)/rumptools/rumpmake
+MACHINE=$(${RUMPMAKE} -f /dev/null -V '${MACHINE}')
+[ -z "${MACHINE}" ] && die could not figure out target machine
+
+# build rump kernel
+./buildrump.sh/buildrump.sh ${BUILD_QUIET} ${STDJ} -k \
+    -V MKPIC=no -s ${RUMPSRC} -T rumptools -o rumpobj -N \
+    -V RUMP_KERNEL_IS_LIBC=1 "$@" build kernelheaders install
+
+LIBS="$(stdlibs ${RUMPSRC})"
+usermtree rump
+userincludes ${RUMPSRC} ${LIBS}
+
+for lib in ${LIBS}; do
+	makeuserlib ${lib}
+done
+
+eval ${BUILDXENMETAL_PCI_P} && makepci ${RUMPSRC}
+exit 0
