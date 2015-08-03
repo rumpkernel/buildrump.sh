ad55254a983da13539f84a44dd42b2c62f623915
Antti Kantee <pooka@iki.fi>
2015-08-03 10:19:27 +0000

    update copyright info

--- buildrump.sh
+++ buildrump.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env sh
 #
-# Copyright (c) 2013 Antti Kantee <pooka@iki.fi>
+# Copyright (c) 2013, 2014, 2015 Antti Kantee <pooka@rumpkernel.org>
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
